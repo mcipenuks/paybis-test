d0321824a627d8c267d9c4fefddaa3100704f6df
Mareks Cipenuks <mcipenuks@gmail.com>
2022-05-14 16:35:41 +0300

    Add loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="spinner"></div>
+</template>
+
+<style lang="scss" scoped>
+.spinner {
+    width: 24px;
+    height: 24px;
+    top: calc(50% - 12px);
+    right: 0;
+    border-radius: 100%;
+    border: 2px solid #fff;
+    border-right: 2px solid transparent;
+    animation: spin .5s linear infinite;
+}
+
+@keyframes spin {
+    to {transform: rotate(360deg);}
+}
+
+</style>
