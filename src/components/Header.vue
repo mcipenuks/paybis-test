4890ba586e6e70ac8e8a9c19047876ecce7ddcef
Mareks Cipenuks <mcipenuks@gmail.com>
2022-05-14 19:27:20 +0300

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header">
+        <LocaleSwitcher/>
+    </div>
+</template>
+
+<script>
+import LocaleSwitcher from '@/components/LocaleSwitcher.vue';
+export default {
+   components: {
+       LocaleSwitcher,
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.3);
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
