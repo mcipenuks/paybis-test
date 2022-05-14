54fa368cbb0084e089f4f7d499b0ecb841d52ac4
Mareks Cipenuks <mcipenuks@gmail.com>
2022-05-14 16:35:15 +0300

    Add constants file

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+export const TICKER_RATE = 3000;
+
+export const INPUT_TYPE_FIAT = 'fiat';
+
+export const INPUT_TYPE_CRYPTO = 'crypto';
+
+export const CRYPTO_CURRENCIES = [
+    { code: 'BTC', name: 'Bitcoin', type: 'crypto' },
+    { code: 'ETH', name: 'Ethereum', type: 'crypto' },
+    { code: 'LTC', name: 'Litecoin', type: 'crypto' }
+];
+
+export const FIAT_CURRENCIES = [
+    { code: 'USD', name: 'United States Dollar', type: 'fiat' },
+    { code: 'EUR', name: 'Euro', type: 'fiat' }
+];
