a51a96ecb6adf53477a230054ef4db430be93cc2
Mareks Cipenuks <mcipenuks@gmail.com>
2022-05-14 16:34:57 +0300

    Add outside directive

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router';
+import { clickOutside } from '@/directives';
 
 const app = createApp(App);
+app.directive('click-outside', clickOutside);
 app.use(router)
 app.mount('#app');
